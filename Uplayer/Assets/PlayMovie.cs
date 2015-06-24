0dccd8b9219827d6b6a71e6c492f8ac70ebbca8d
Ares Lee <fireares@hotmail.com>
2015-06-24 10:35:42 -0700

    added the video playback function

---
+++ Uplayer/Assets/PlayMovie.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PlayMovie : MonoBehaviour {
+	public static MovieTexture theMovie;
+	public static AudioSource theAudio;
+
+	// Use this for initialization
+	void Start () {
+		//load the video and its audio component
+		loadVideo ();
+		loadAudio (theMovie);
+
+		//play the video with its audio
+//		theMovie.Play();
+//		GetComponent<AudioSource>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void loadVideo(){
+		Renderer r = GetComponent<Renderer>();
+		theMovie = (MovieTexture)r.material.mainTexture;
+		GameObject.Find("Plane").AddComponent<AudioSource>();
+	}
+
+
+	void loadAudio(MovieTexture _videoTexture){
+
+		theAudio=GameObject.Find("Plane").GetComponent<AudioSource>();
+		theAudio.clip=_videoTexture.audioClip;
+
+	}
+}
---
+++ Uplayer/Assets/btnScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btnScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void playTheVideo(){
+		PlayMovie.theMovie.Play();
+		PlayMovie.theAudio.Play ();
+
+
+	}
+}
